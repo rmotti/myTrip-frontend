4ff9505391bd154f2279b1dc547a00b9f013e630
Rodrigo Motti de Santana <113243243+rmotti@users.noreply.github.com>
2025-10-23 16:03:56 -0300

    style: home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,20 @@
 import { useTrips } from "../hooks/useTrips";
 
+
 export default function Home() {
-  const { trips, loading } = useTrips();
+  const { trips, loading} = useTrips();
 
   return (
     <div className="p-6 bg-[#f0fbff] min-h-screen">
       <header className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold text-[#0b3b59]">
-          Planejador de Viagens
+          MyTrip - Suas Viagens Organizadas
         </h1>
         <button className="bg-[#0b3b59] text-white rounded-lg px-4 py-2 hover:bg-[#115a84] transition">
           + Nova Viagem
         </button>
       </header>
 
-      {/* Cards do topo */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-        <div className="bg-white rounded-2xl p-4 shadow-sm">
-          <h2 className="text-gray-600 text-sm">Orçamento Total</h2>
-          <p className="text-2xl font-semibold text-[#0b3b59]">R$ 23.000,00</p>
-          <p className="text-xs text-gray-500">Planejado: R$ 23.000,00</p>
-        </div>
-
-        <div className="bg-white rounded-2xl p-4 shadow-sm">
-          <h2 className="text-gray-600 text-sm">Total Gasto</h2>
-          <p className="text-2xl font-semibold text-[#0b3b59]">R$ 10.600,00</p>
-          <p className="text-xs text-gray-500">46.1% do planejado</p>
-        </div>
-
-        <div className="bg-white rounded-2xl p-4 shadow-sm">
-          <h2 className="text-gray-600 text-sm">Viagens</h2>
-          <p className="text-2xl font-semibold text-[#0b3b59]">
-            {trips.length} próximas
-          </p>
-        </div>
-      </div>
 
       {/* Lista de viagens dinâmicas */}
       <section>
@@ -53,7 +33,7 @@ export default function Home() {
                 key={trip.id}
                 className="bg-white rounded-2xl overflow-hidden shadow-md"
               >
-                <img
+                {/* <img
                   src={
                     trip.name.toLowerCase().includes("paris")
                       ? "/img/paris.jpg"
@@ -63,18 +43,18 @@ export default function Home() {
                   }
                   alt={trip.name}
                   className="h-48 w-full object-cover"
-                />
+                />*/}
                 <div className="p-4">
                   <h3 className="text-lg font-semibold text-[#0b3b59]">
                     {trip.name}
                   </h3>
-                  <p className="text-sm text-gray-600">
+                  <p className="text-md text-gray-600">
                     📅 {trip.start_date} → {trip.end_date}
                   </p>
-                  <p className="text-sm text-gray-600">
-                    💰 {trip.currency_code} {trip.total_budget?.toFixed(2)}
+                  <p className="text-md text-gray-600">
+                    Meta 💰 → {trip.total_budget?.toFixed(2)}
                   </p>
-                  <button className="text-red-600 text-sm mt-3 flex items-center gap-1 hover:underline">
+                  <button className="text-red-600 text-md mt-3 flex items-center gap-1 hover:underline">
                     🗑 Excluir
                   </button>
                 </div>
@@ -82,23 +62,6 @@ export default function Home() {
             ))}
           </div>
         )}
-      </section>
-
-      {/* Gráficos (mock) */}
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
-        <div className="bg-white rounded-2xl p-4 shadow-sm">
-          <h3 className="font-semibold mb-2 text-[#0b3b59]">
-            Distribuição de Gastos
-          </h3>
-          <p className="text-gray-500 text-sm">Gráfico mockado por enquanto.</p>
-        </div>
-
-        <div className="bg-white rounded-2xl p-4 shadow-sm">
-          <h3 className="font-semibold mb-2 text-[#0b3b59]">
-            Planejado vs. Realizado
-          </h3>
-          <p className="text-gray-500 text-sm">Gráfico mockado por enquanto.</p>
-        </div>
       </section>
     </div>
   );
